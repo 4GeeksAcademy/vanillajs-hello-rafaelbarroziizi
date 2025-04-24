d37f585f54ce5208b932f23b711d9545dec0d716
Rafaelbarrozzi <rafaeljosebarrozzi@gmail.com>
2025-04-24 03:33:30 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("excuse").innerHTML = generalexcuse()
+}
+  function generalexcuse (){
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    let pronoun = Math.floor(Math.random()*who.length)
+    let accion = Math.floor(Math.random()*action.length)
+    let que = Math.floor(Math.random()*what.length)
+    let cuando = Math.floor(Math.random()*when.length)
+    return (who[pronoun]+" "+action[accion]+" "+what[que]+" "+when[cuando])
+    
+  }
